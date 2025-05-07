98dcaa28d6a1860a0dcfafb144adeedc9483f666
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-05-07 14:00:05 +0200

    added terraform module files

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "location" {
+  default = "West Europe"
+}
+
+variable "resource_group_name" {
+  default = "myresourcegroupname"
+}
+
+variable "storage_account_name" {
+  default = "mystorageaccount"
+}
+
